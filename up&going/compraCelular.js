3cec7eb4304d36eb64f84a87f813fb279e3b5e84
Eduardo <thiago.novak7@gmail.com>
2020-10-19 18:18:59 -0300

    first commit

---
+++ up&going/compraCelular.js
@@ -0,0 +1,20 @@
+/*
+Fazer a parte pratica
+Cáp 1
+
+*/
+
+const precoCelular = 0;
+const limiteGastos = 0;
+const taxaImposto = 0;
+const precoAcessorio = 0;
+
+let saldoBancario = 0;
+
+
+
+function compraEfetuada(){
+    console.log("comprou")
+}
+
+compraEfetuada()
